a52e3c4272a1fd080709f0df6538684fc1f22464
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-08-27 13:30:32 +0200

    Add R/W CSV

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ const getConfig = require('../src/config/config-mgr');
 const start = require('../src/commands/start.js');
 const todo = require('../src/commands/todo')
 
-try {
+//try {
     const args = arg({
     '--todo': Boolean,
     '--build': Boolean,
@@ -16,13 +16,15 @@ try {
     if (args["--todo"]){
         // const config = getConfig()
         // start(config)
-        todo();
+        console.log("TODO")
+        todo.readTodoList()
+        console.log("Called")
     } 
 
-}   catch (e) {
+//}   catch (e) {
     logger.debug(e.message + "\n");
     usage();
-}
+//}
 function usage(){
     logger.warning(`${chalk.whiteBright('tool [CMD]')}
     ${chalk.greenBright('--start')}\tStarts the app
--- src/commands/todo.js
+++ src/commands/todo.js
@@ -1,4 +1,39 @@
-
-module.exports = function todo(){
-    console.log("TODO")
+const csv = require('csv-writer').createObjectCsvWriter
+const csvReader = require('csv-parser')
+const fs = require('fs')
+const csvWriter = csv({
+    path: 'TODO.csv',
+    header: [
+        {id: 'task', title:"Task"},
+        {id: 'isDone', title:"Done"},
+        {id: 'dueDate', title:"Due date"}
+    ]
+});
+if (!fs.existsSync("TODO.csv")){
+    fs.writeFile('TODO.csv', "");
 }
+function saveTodoList(){
+    console.log("TODO")
+
+    const data = [
+        {
+            task: 'Games',
+            isDone: "false",
+            dueDate: "10/10/2002"
+        }
+    ];
+
+    csvWriter
+  .writeRecords(data)
+  .then(()=> console.log('Saved TODO list sucessfully!'));
+
+
+}
+
+function readTodoList(){
+    fs.createReadStream('TODO.csv').pipe(csv()).on('data', (row) => {
+        console.log(row)
+    })
+}
+
+module.exports = {saveTodoList, readTodoList}
