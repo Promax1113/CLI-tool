2edac16600da5ed62ddeba843bd5f4e5d0c2ace5
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-09-16 17:14:28 +0200

    Fix GFN Eta calculator

--- src/utils/gfn.js
+++ src/utils/gfn.js
@@ -1,11 +1,11 @@
 const logger = require("../logger")('utils:gfn')
 
-module.exports = function getETA(peopleNow, peopleStart, timeElapsed){
+module.exports = function gfnETR(peopleNow, peopleStart, timeElapsed){
     
     let vel = (parseInt(peopleStart) - parseInt(peopleNow))/parseInt(timeElapsed)
     logger.warning(`About ${Math.round(vel)} people/minute`)
-    let timeRemaining = vel * parseInt(peopleNow)
+    let timeRemaining = parseInt(peopleNow) / vel
     let formattedTime = `${Math.floor(timeRemaining)} minutes and ${Math.round((timeRemaining - Math.floor(timeRemaining)) * 60)} seconds`
-    logger.highlight(`ETA: ${formattedTime} remaining approximately.`)
+    logger.highlight(`ETR: ${formattedTime} remaining approximately.`)
 
 }
